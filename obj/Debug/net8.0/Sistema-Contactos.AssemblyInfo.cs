41fc4f1f60e81e28255dfd220c4efa7633158709
Erick Pertuz Anaya <epertuza1@unicartagena.edu.co>
2026-08-14 16:10:58 -0500

    Se mejora el diseño visual del login y layout principal

--- obj/Debug/net8.0/Sistema-Contactos.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sistema-Contactos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema-Contactos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089d21a2114e81d72394a3658aa51365f104ee46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c6041f0e6f628d1745702620362ce280fbe2b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema-Contactos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema-Contactos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
